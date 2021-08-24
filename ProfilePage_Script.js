5657f41fbb9e7e6b91b4b0417558cd67f86c893b
Luke Hufnagle <leh17068@gmail.com>
2021-08-24 16:14:15 -0400

    MakingConnections_Assignment

---
+++ ProfilePage_Script.js
@@ -0,0 +1,12 @@
+function remove(){
+    document.querySelector(".row1").remove();
+}
+
+function remove2(){
+    document.querySelector(".row2").remove();
+}
+
+function changeName(){
+    var myName = document.querySelector(".myName");
+    myName.innerText = "Jane Doe"
+}
